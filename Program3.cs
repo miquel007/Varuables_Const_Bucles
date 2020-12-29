4a0050a03315324617f66ee0664812f2ad5bfd9a
Miquel Roig <miquel.roig@urv.cat>
2020-12-29 11:13:41 +0100

    try5

---
+++ Program3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp40
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>();
+
+            for (int i = 1; i < 11; i++)
+                array.Add(i);
+
+            foreach (int num in array)
+                Console.WriteLine(num);
+
+            Console.WriteLine("\n");
+
+            int auxiliar = 0;
+
+            for (int j = 0; j < 5; j++ )
+            {
+                auxiliar = array[j];
+
+                array[j] = array[9 - j];
+
+                array[9 - j] = auxiliar;
+            }
+
+            foreach (int num in array)
+                Console.WriteLine(num);
+        }
+    }
+}
