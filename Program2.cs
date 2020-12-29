d33975955d5de184e6f4d9a92c72d21206b88672
Miquel Roig <miquel.roig@urv.cat>
2020-12-29 11:00:41 +0100

    try3

---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double num = 1234.3456;
+            int a = (int)num;
+            float b = (float)num;
+            string c = num.ToString();
+
+
+            Console.WriteLine("\n El double es {0}  \n El int es {1}  \n El float es {2}  \n El string es {3}", num, a, b, c);
+
+        }
+    }
+}
